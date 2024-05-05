8842546f3ef8c61f7a44b1bffb642289eb417063
bon1254 <bon1254@gmail.com>
2024-05-06 00:52:07 +0800

    更新調整

--- BookSelling/wwwroot/js/product.js
+++ BookSelling/wwwroot/js/product.js
@@ -8,20 +8,20 @@ function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "ajax": { url: '/admin/product/getall' },
         "columns": [
-            { data: 'title', "width": "25%" },
+            { data: 'title', "width": "15%" },
             { data: 'isbn', "width": "15%" ,className: 'dt-body-left' },
-            { data: 'listPrice', "width": "15%", className: 'dt-body-left' },
-            { data: 'author', "width": "25%" },
+            { data: 'listPrice', "width": "8%", className: 'dt-body-left' },
+            { data: 'author', "width": "15%" },
             { data: 'category.name', "width": "15%" },
             {
                 data: 'id',
                 "render": function (data) {
-                    return `<div class="w-75 btn-group" role="group">
+                    return `<div class="w-100 btn-group" role="group">
                         <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i>編輯</a> 
                         <a onClick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2"><i class="bi bi-trash-fill"></i>刪除</a>
                         </div>`
                 },
-                "width": "25%"
+                "width": "20%"
             }
         ]
     });
